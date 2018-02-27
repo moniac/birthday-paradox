a372e7c175433b48c4582fed7aa5dddffc53b904
Alex van der wal <amvvanderwal@live.nl>
2018-02-27 13:02:16 +0100

    added person object

--- app.js
+++ app.js
@@ -4,6 +4,14 @@
             helpers.getInput()
         }
     }
+
+    const person = {
+        date: 0,
+        generateDate: function(start = new Date(2012, 0, 1), end = new Date()) {
+            this.date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+        }
+    }
+
     const helpers = {
         getInput() {
             document.querySelector('body > input').addEventListener('keyup', function(e) {
