ee8c6a7556ee8dc114ce862e88208120adc2171f
Mohammed "Mo" Mulazada <thisismoniac@gmail.com>
2018-02-27 12:55:04 +0100

    Added app init and input log

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('test')
+(function() {
+    const app = {
+        init() {
+            helpers.getInput()
+        }
+    }
+    const helpers = {
+        getInput() {
+            document.querySelector('body > input').addEventListener('keyup', function(e) {
+                console.log(e.target.value)
+            })
+        }
+    }
+    app.init()
+})()
